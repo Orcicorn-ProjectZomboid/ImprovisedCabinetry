7791e59e3af2b2d3ab212fc8fd20b0c892ef0427
Orcicorn <orcicorn@gmail.com>
2022-05-20 09:13:42 -0400

    41.71 Recipe XP Updates

--- Contents/mods/ImprovisedCabinetry/media/lua/server/ICRecipeCode.lua
+++ Contents/mods/ImprovisedCabinetry/media/lua/server/ICRecipeCode.lua
@@ -1,16 +1,16 @@
 -- Give XP on Completion of Cabinets
-function ImprovisedCabinetry_XPWoodCabine(recipe, ingredients, result, player)
+function Recipe.OnGiveXP.ImprovisedCabinetry_XPWoodCabine(recipe, ingredients, result, player)
     player:getXp():AddXP(Perks.Woodwork, 10);
 end
 
-function ImprovisedCabinetry_XPMetalCabinet(recipe, ingredients, result, player)
+function Recipe.OnGiveXP.ImprovisedCabinetry_XPMetalCabinet(recipe, ingredients, result, player)
     player:getXp():AddXP(Perks.MetalWelding, 10);
 end
 
-function ImprovisedCabinetry_XPWoodPanel(recipe, ingredients, result, player)
+function Recipe.OnGiveXP.ImprovisedCabinetry_XPWoodPanel(recipe, ingredients, result, player)
     player:getXp():AddXP(Perks.Woodwork, 5)
 end
 
-function ImprovisedCabinetry_XPMetalHinge(recipe, ingredients, result, player)
+function Recipe.OnGiveXP.ImprovisedCabinetry_XPMetalHinge(recipe, ingredients, result, player)
     player:getXp():AddXP(Perks.MetalWelding, 5)
 end
